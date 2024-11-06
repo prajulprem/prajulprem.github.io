9820cacfd73b0b2dc2f3d17cca489cc8e8c13bab
prajulprem <prajulprem017@gmail.com>
2024-11-06 19:27:43 +0530

    updated next.config.mjson

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-// next.config.js
-module.exports = {
-  output: 'export',
-  // You may also need to set the basePath if your repo name is different
-  basePath: '/prajulprem.github.io', // Replace with your GitHub repo name
-  trailingSlash: true,
-};
 
 export default nextConfig;
